37f3764ac3e793e176e296672a7b1509c7da2773
Haroon Ahmad <haroonfm2005@gmail.com>
2025-12-13 01:06:01 +0500

    First Commit

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def main():
     args = parse_args()
     print(f"Using seed ===> {args.seed}")
     set_seed(args.seed)
+    
 
     # Validate domain configuration for domain-based datasets
     if args.tsne_plot:
